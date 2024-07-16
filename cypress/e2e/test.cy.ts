baf4b4a578563acb9da1067655fd3a03390a565a
Gowtham-Neo <s.gowtham.engr@gmail.com>
2024-07-16 15:51:26 +0530

    updated cypress

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -10,37 +10,6 @@ describe('Sport Centre Application Tests', () => {
         cy.url().should('include', '/');
     });
 
-    it('Sign-in Successfull', () => {
-      cy.visit('https://sport-centre.netlify.app/signin', { timeout: 50000 })
-
-      cy.get('input[name="email"]').type('correct.user@example.com');
-      cy.get('input[name="password"]').type('user.password');
-      cy.get('form').submit();
-  
-      cy.url().should('include', '/');
-    });
-
-    it('Displays error message for invalid credentials', () => {
-      cy.visit('https://sport-centre.netlify.app/signin', { timeout: 50000 })
-
-      cy.get('input[name="email"]').type('invalid@example.com');
-      cy.get('input[name="password"]').type('invalidpassword');
-      cy.get('form').submit();
-  
-      cy.contains('Invalid credentials !').should('be.visible');
-    });
-    
-    it('Navigates to /signin after clearing local storage', () => {
-        localStorage.setItem('auth_token', 'mockAuthToken');
-        localStorage.setItem('user', JSON.stringify({ id: 1, username: 'testuser' }));
-        localStorage.setItem('selectedSports', JSON.stringify(['football', 'basketball']));
-        localStorage.setItem('selectedTeams', JSON.stringify(['teamA', 'teamB']));
-
-        cy.visit('https://sport-centre.netlify.app/logout');
-        cy.url().should('include', '/logout');
-        cy.wait(500); 
-        cy.url().should('include', '/signin');
-      });
 
       
       it('should have the correct title', () => {
