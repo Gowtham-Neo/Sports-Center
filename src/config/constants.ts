8f61ad94c06e47ed22b9342edc0316a33926a811
Gowtham-Neo <s.gowtham.engr@gmail.com>
2024-08-05 20:54:09 +0530

    Generative Ai Update

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,1 @@
 export const API_ENDPOINT = "https://wd301-capstone-api.pupilfirst.school/";
-// export const AI_API =
-//   "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";
-
-export const apiKey = "AIzaSyC1gfKO5Lr2zcQlPco-NG69Bsv_Oz7_u4U"; 
